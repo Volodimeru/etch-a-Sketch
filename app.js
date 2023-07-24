865d8bf10e9e40a60d1f5e84dedc47e0f17c64f3
volodimeru <vladi.dogonadze@gmail.com>
2023-07-24 14:08:10 +0400

    Almoast finished Javascript file

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const everyDiv = document.querySelectorAll("div")
-console.log(everyDiv)
-everyDiv.forEach(div =>div.addEventListener('mouseover', function() {
-    everyDiv.classList.add('onmo')
-}));
+const divsContainer = document.querySelector(".container");
+inputButt = document.querySelector(".inputButton");
+
+inputButt.addEventListener('click', function() {
+    divsContainer.innerHTML="";
+    let divsQuantity = prompt("Enter number no more than 30");
+    if (divsQuantity < 31) {
+for(i=0; i<divsQuantity*divsQuantity; i++) {
+    const newDiv = document.createElement('div');
+    divsContainer.append(newDiv);
+    newDiv.classList.add("with");
+    divsContainer.style.gridTemplateColumns = `repeat(${divsQuantity}, 1fr)`
+    divsContainer.style.gridTemplateRows = `repeat(${divsQuantity}, 1fr)`
+    const allTheDivs = document.querySelectorAll('.with');
+    allTheDivs.forEach(div => div.addEventListener('mouseover', function() {
+    div.classList.add('addedClass');
+    // If uncomment line bellow color will be changed with styling method
+    // div.style.backgroundColor = "red"; 
+}));}}})
